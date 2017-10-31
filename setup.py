ac637b34f836094a08a4a4d7a01db989ad32d07a
Bryan Irvine <sparctacus@gmail.com>
2017-10-31 11:32:00 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.community.Infoblox"
-VERSION = "0.0.1"
+VERSION = "0.0.2"
 AUTHOR = "Bryan Irvine"
 LICENSE = "MIT"
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.community']
